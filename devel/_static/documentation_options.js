68514d1e450d953bbd88e656318c62bf2a7ce900
Github <no-reply@github.com>
2020-10-16 15:42:58 +0000

    Update documentation for devel
    Auto-generated from v0.4.0-103-g8b42bde by 'update-gh-pages.sh'

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0-85-gf99b0e3',
+    VERSION: '0.4.0-103-g8b42bde',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ releases/versions.js
@@ -0,0 +1,16 @@
+function getVersionsMenuItems() {
+  return [
+    { name: '_static', url: '/cri-resource-manager/_static' },
+    { name: 'cmd', url: '/cri-resource-manager/cmd' },
+    { name: 'demo', url: '/cri-resource-manager/demo' },
+    { name: 'devel', url: '/cri-resource-manager/devel' },
+    { name: 'dockerfiles', url: '/cri-resource-manager/dockerfiles' },
+    { name: 'docs', url: '/cri-resource-manager/docs' },
+    { name: 'elf', url: '/cri-resource-manager/elf' },
+    { name: 'packaging', url: '/cri-resource-manager/packaging' },
+    { name: 'pkg', url: '/cri-resource-manager/pkg' },
+    { name: 'sample-configs', url: '/cri-resource-manager/sample-configs' },
+    { name: 'scripts', url: '/cri-resource-manager/scripts' },
+    { name: 'test', url: '/cri-resource-manager/test' },
+  ];
+}
